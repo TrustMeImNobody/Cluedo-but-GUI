549a0e7fa33c2cb79c727d379e6382b181a10611
floatedsugar240 <53988682+floatedsugar240@users.noreply.github.com>
2020-08-27 19:43:46 +1200

    Added Icon class

---
+++ src/Icon.java
@@ -0,0 +1,122 @@
+import java.awt.*;
+
+public class Icon {
+
+    int xPos;
+    int yPos;
+    int width = 20;
+    int height = 20;
+    String text;
+    Color color;
+
+    public Icon(String text){
+        switch(text){
+            //Character Icon and start Position Setup
+            case "Colonel Mustard":
+                this.xPos = 32;
+                this.yPos = 324;
+                this.color = Color.orange;
+                this.text = "M";
+                break;
+            case "Miss Scarlett":
+                this.xPos = 195;
+                this.yPos = 452;
+                this.color = Color.red;
+                this.text = "S";
+                break;
+            case "Mrs. White":
+                this.xPos = 241;
+                this.yPos = 16;
+                this.color = Color.white;
+                this.text = "W";
+                break;
+            case "Mr. Green":
+                this.xPos = 357;
+                this.yPos = 16;
+                this.color = Color.green;
+                this.text = "G";
+                break;
+            case "Mrs. Peacock":
+                this.xPos = 566;
+                this.yPos = 124;
+                this.color = Color.blue;
+                this.text = "Pe";
+                break;
+            case "Professor Plum":
+                this.xPos = 566;
+                this.yPos = 360;
+                this.color = Color.PINK;
+                this.text = "Pl";
+                break;
+            //
+            //Weapon Icon setup and start position
+            //
+            case "Candlestick":
+                //this.xPos = 32;
+                //this.yPos = 324;
+                this.color = Color.gray;
+                this.text = "C";
+                break;
+            case "Dagger":
+                //this.xPos = 195;
+                //this.yPos = 452;
+                this.color = Color.gray;
+                this.text = "D";
+                break;
+            case "Lead Pipe":
+                //this.xPos = 241;
+                //this.yPos = 16;
+                this.color = Color.gray;
+                this.text = "L";
+                break;
+            case "Revolver":
+                //this.xPos = 357;
+                //this.yPos = 16;
+                this.color = Color.gray;
+                this.text = "Re";
+                break;
+            case "Rope":
+                //this.xPos = 566;
+                //this.yPos = 124;
+                this.color = Color.gray;
+                this.text = "Ro";
+                break;
+            case "Spanner":
+                //this.xPos = 566;
+                //this.yPos = 360;
+                this.color = Color.GRAY;
+                this.text = "Sp";
+                break;
+        }
+
+    }
+
+    public void move(int dx, int dy){
+        this.xPos += dx;
+        this.yPos += dy;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
